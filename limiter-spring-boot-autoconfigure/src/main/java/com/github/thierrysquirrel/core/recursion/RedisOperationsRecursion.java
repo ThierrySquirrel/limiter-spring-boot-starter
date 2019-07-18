1d4a414dbf52cebce2d1570cefd7765cadcdb56e
ThierrySquirrel <2161271173@qq.com>
2019-07-18 13:10:53 +0800

    first commit

---
+++ limiter-spring-boot-autoconfigure/src/main/java/com/github/thierrysquirrel/core/recursion/RedisOperationsRecursion.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.thierrysquirrel.core.recursion;
+
+
+import com.github.thierrysquirrel.core.execution.RedisOperationsExecution;
+import lombok.Data;
+import org.springframework.data.redis.core.BoundListOperations;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * ClassName: RedisOperationsRecursion
+ * Description:
+ * date: 2019/7/17 19:06
+ *
+ * @author ThierrySquirrel
+ * @since JDK 1.8
+ */
+@Data
+public class RedisOperationsRecursion {
+	private AtomicLong offset;
+	private Long maximumCapacity;
+
+	public RedisOperationsRecursion(AtomicLong offset, Long maximumCapacity) {
+		this.offset = offset;
+		this.maximumCapacity = maximumCapacity;
+	}
+
+	public void addTokens(BoundListOperations<Object, Object> boundListOperations, Integer tokenValue) {
+
+		Long currentQuantity = RedisOperationsExecution.addTokens(boundListOperations, tokenValue, maximumCapacity);
+		long thisOffset = offset.get();
+		if (maximumCapacity > currentQuantity && thisOffset > 0) {
+			Long update = thisOffset - 1;
+			offset.compareAndSet(thisOffset, update);
+			addTokens(boundListOperations, tokenValue);
+		} else {
+			offset.compareAndSet(thisOffset, 0);
+		}
+	}
+}
